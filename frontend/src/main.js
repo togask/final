a72caf899e394d6f4b50de43023e14859a98b432
togask <141380453+togask@users.noreply.github.com>
2024-01-15 10:06:10 +0900

    axiosのパス設定

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import axios from "axios";
 
-createApp(App).mount('#app')
+axios.defaults.baseURL = "http://127.0.0.1:8000";
+
+createApp(App).mount("#app");
