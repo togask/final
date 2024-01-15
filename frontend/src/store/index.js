5b053072bcafeba96149253fc004576dd5f57c8f
togask <141380453+togask@users.noreply.github.com>
2024-01-15 14:08:04 +0900

    Vuexの設定

---
+++ frontend/src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+  state: {
+    // ステート（データ）を定義
+  },
+  mutations: {
+    // ミューテーション（状態の変更）を定義
+  },
+  actions: {
+    // アクション（非同期処理）を定義
+  },
+});
+
+export default store;
