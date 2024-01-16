51039fdc681824e9769f1c9a3bd43649b2d21923
togask <141380453+togask@users.noreply.github.com>
2024-01-16 15:42:22 +0900

    本番環境のパスに変更

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import store from "./store";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://127.0.0.1:8000";
+axios.defaults.baseURL = "https://aso-2201402.main.jp/yogurt/backend/public/";
 
 const app = createApp(App);
 app.use(store);
