2d0880e84ed0fc9eaed48125e315966dba25f452
togask <141380453+togask@users.noreply.github.com>
2024-01-17 09:37:32 +0900

    PublicPathの指定

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+});
+module.exports = {
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/datemi_farm/" // 本番用のパス
+      : "/", // 開発環境用
+};
